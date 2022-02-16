2886896b520760bf0bf1c1f45ef04266db8b5dd6
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-02-16 11:43:35 +0900

    Cube and IndexedLineList Implemented

---
+++ Engine/Cube.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Vec3.h"
+#include "IndexedLineList.h"
+#include <vector>
+
+class Cube
+{
+public:
+	Cube( float size )
+	{
+		vertices.reserve( 8 );
+		vertices.emplace_back( size, size, size );
+		vertices.emplace_back( size, -size, size );
+		vertices.emplace_back( -size, size, size );
+		vertices.emplace_back( -size, -size, size );
+		vertices.emplace_back( size, size, -size );
+		vertices.emplace_back( size, -size, -size );
+		vertices.emplace_back( -size, size, -size );
+		vertices.emplace_back( -size, -size, -size );
+	}
+	IndexedLineList GetLines() const
+	{
+		return
+		{
+			vertices,
+			{
+				0,1, 0,2, 1,3, 2,3,
+				2,4, 3,5, 4,5, 4,6,
+				5,7, 6,7, 6,0, 7,1
+			}
+		}
+	}
+private:
+	std::vector<Vec3> vertices
+};
---
+++ Engine/IndexedLineList.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Vec3.h"
+#include <vector>
+
+struct IndexedLineList
+{
+	std::vector<Vec3> vertices;
+	std::vector<size_t> indices;
+};
