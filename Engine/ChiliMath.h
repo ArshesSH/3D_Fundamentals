e5b948fef9e89ccf0ccc5536e7f48a7978e3f7ac
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-02-17 12:49:05 +0900

    Implemented wrap_angle function

--- Engine/ChiliMath.h
+++ Engine/ChiliMath.h
@@ -23,9 +23,25 @@
 #include <math.h>
 
 constexpr float PI = 3.14159265f;
+constexpr float PI_D = 3.1415926535897932;
 
 template <typename T>
 inline auto sq( const T& x )
 {
 	return x * x;
+}
+
+template <typename T>
+inline T wrap_angle( T theta )
+{
+	const T modded = fmod( theta, (T)2.0 * (T)PI_D );
+	if ( modded > (T)PI_D )
+	{
+		return modded - (T)2.0 * (T)PI_D;
+	}
+	else if ( modded < -(T)PI_D )
+	{
+		return modded + (T)2.0 * (T)PI_D;
+	}
+	return modded;
 }
